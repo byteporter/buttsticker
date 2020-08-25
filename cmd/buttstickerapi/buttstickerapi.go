2848775f563aeb720fa50f83c153ecca94071688
James <james@byteporter.com>
2020-08-25 16:50:32 -0500

    refactor

--- cmd/buttstickerapi/buttstickerapi.go
+++ cmd/buttstickerapi/buttstickerapi.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "fmt"
-//    "html/template"
+    //    "html/template"
     "log"
     "net/http"
     "path/filepath"
@@ -12,34 +12,46 @@ import (
     "io/ioutil"
     "math/rand"
 
-//    "github.com/gorilla/handlers"
+    //    "github.com/gorilla/handlers"
     "github.com/gorilla/mux"
 )
 
 type buttstickerHandler struct {
     h           http.Handler
-    contentDir  string
+    tickers     []string
 }
 
 func (bh buttstickerHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-    tickerJson, err := os.Open(filepath.Join(bh.contentDir, "tickers.json"))
-    if err != nil {
-        fmt.Fprintf(w, err.Error())
-    }
-
-    byteVal, _ := ioutil.ReadAll(tickerJson)
-    var tickers []string
-    json.Unmarshal(byteVal, &tickers)
-
-    fmt.Fprintf(w, tickers[rand.Intn(4)])
-
-    defer tickerJson.Close()
+    fmt.Fprintf(w, bh.tickers[rand.Intn(len(bh.tickers))])
 }
 
 func main() {
-    router := mux.NewRouter()
-    router.Handle("/", buttstickerHandler{nil, "/usr/share/tickerdata"}).Methods("GET")
-    http.Handle("/", router)
+    var bh buttstickerHandler
 
+    tickerJson, err := os.Open(filepath.Join("/usr/share/tickerdata", "tickers.json"))
+    if err != nil {
+        log.Println(err.Error())
+    }
+    byteVal, _ := ioutil.ReadAll(tickerJson)
+    json.Unmarshal(byteVal, &bh.tickers)
+    defer tickerJson.Close()
+
+    apiPrefix := "/api/v1"
+    router := mux.NewRouter()
+    apiRouter := router.PathPrefix(apiPrefix).Subrouter()
+
+    apiRouter.Handle("/tickers/rand", bh).Methods("GET")
+
+    http.Handle("/", apiRouter)
+
+    log.Println("*** INFO: Available Routes:")
+    apiRouter.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+        t, err := route.GetPathTemplate()
+        if err != nil {
+            return err
+        }
+        log.Println("*** INFO: " + t)
+        return nil
+    })
     log.Fatal(http.ListenAndServe(strings.Join([]string{":", "80"}, ""), router));
 }
